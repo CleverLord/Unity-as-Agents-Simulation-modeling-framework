850d6abb9bfd983810d013f02122ddd83716dbe7
Jan Karpiuk <karpiukjan@student.agh.edu.pl>
2024-01-16 00:54:31 +0100

    Correct line chart scaling to max visible data

--- LotkaVoltera/Assets/Scripts/Metrics/LineChart.cs
+++ LotkaVoltera/Assets/Scripts/Metrics/LineChart.cs
@@ -1,9 +1,8 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.Purchasing;
 
 namespace TerrainGeneration
 {
@@ -40,8 +39,9 @@ namespace TerrainGeneration
         public int dataLinesCount;
 
         // Chart data
+        private float[] maxLineValues;
         private List<float>[] dataPoints;
-        private float maxVisibleValue;
+        private int maxVisibleValueLineIndex = 0;
 
         // Drawing the chart
         // which data lines should the chart not show 
@@ -58,6 +58,7 @@ namespace TerrainGeneration
         // Start is called before the first frame update
         void Start()
         {
+            maxLineValues = new float[lineRenderers.Length];
             // scale chart viewport to wordl size if scaler object exists
             if (chartToWorldScaler != null)
                 chartToWorldScaler.scaleViewportToWorld();
@@ -147,7 +148,7 @@ namespace TerrainGeneration
             xAxisScaleTooltip.text = $"{dataPoints[0].Count:F2}";
 
             // Update Y-axis data text based on the current maxVisibleValue
-            yAxisScaleTooltip.text = $"{maxVisibleValue:F2}";
+            yAxisScaleTooltip.text = $"{maxLineValues[maxVisibleValueLineIndex]:F2}";
         }
 
         // register new data points to apropriate indexes
@@ -159,37 +160,81 @@ namespace TerrainGeneration
             if (newDataEntry.Count == 0)
                 return;
 
+            // update max value based on new data and visible datalines
             for (int lineIdx = 0; lineIdx < lineRenderers.Length; lineIdx++)
             {
                 if (hideLines.Contains(lineIdx))
                     continue;
 
                 float newValue = newDataEntry.ElementAt(lineIdx).Value;
-                UpdateLineDataPoints(lineIdx, newValue);
+                UpdateLineDataMaxVisibleVal(lineIdx, newValue);
+            }
+
+            // update visible data lines
+            for (int lineIdx = 0; lineIdx < lineRenderers.Length; lineIdx++)
+            {
+                if (hideLines.Contains(lineIdx))
+                    continue;
+
+                UpdateLineDataPoints(lineIdx);
             }
         }
 
-        private void UpdateLineDataPoints(int lineIdx, float newValue)
+        private void UpdateLineDataMaxVisibleVal(int lineIdx, float newValue)
         {
             // add new value to line
             dataPoints[lineIdx].Add(newValue);
             lineRenderers[lineIdx].positionCount += 1;
 
-            // if necesary set new max visible dataPoint value
-            if (!hideLines.Contains(lineIdx) && maxVisibleValue < newValue)
+            // if line is hidden then set max visible value for line as 0
+            if (hideLines.Contains(lineIdx))
             {
-                maxVisibleValue = newValue;
+                maxLineValues[lineIdx] = 0;
+                return;
+                // if necesary set new max visible dataPoint value
             }
+            else if (!hideLines.Contains(lineIdx) && maxLineValues[lineIdx] < newValue)
+            {
+                maxLineValues[lineIdx] = newValue;
+                
+                if (lineIdx != maxVisibleValueLineIndex && maxLineValues[maxVisibleValueLineIndex] < newValue)
+                    maxVisibleValueLineIndex = lineIdx;
+            }
+        }
 
+        private void UpdateLineDataPoints(int lineIdx)
+        {
             // set line renderer points to apropriate chart values
             for (int dataPointIdx = 0; dataPointIdx < dataPoints[lineIdx].Count; dataPointIdx++)
             {
                 float x = width * dataPointIdx / dataPoints[lineIdx].Count;
                 x += lineRenderers[lineIdx].transform.localPosition.x;
-                float y = height * dataPoints[lineIdx][dataPointIdx] / maxVisibleValue;
+                float y = height * dataPoints[lineIdx][dataPointIdx];
+                if (maxLineValues[maxVisibleValueLineIndex] == 0)
+                {
+                    y /= 1;
+                } else
+                {
+                    y /= maxLineValues[maxVisibleValueLineIndex];
+                }
+                
                 y += lineRenderers[lineIdx].transform.localPosition.y;
                 lineRenderers[lineIdx].SetPosition(dataPointIdx, new Vector3(x, y, 0));
             }
+        }
+
+        private float GetMaxVisibleValue()
+        {
+            float maxVisibleValue = 0;
+            for (int idx = 0; idx < dataLines.Length; idx++)
+            {
+                if (hideLines.Contains(idx))
+                    continue;
+                float currLineMax = dataPoints[idx].Max();
+                if (currLineMax > maxVisibleValue)
+                    maxVisibleValue = currLineMax;
+            }
+            return maxVisibleValue;
         }
 
         private void DrawChart()
@@ -208,18 +253,22 @@ namespace TerrainGeneration
                     // if line is beeing hidden
                     if (nextLineState == false)
                     {
+                        // so max visible data is zero
+                        maxLineValues[idx] = 0;
                         // set curr max visible value to max value of the still visible lines
-                        float currMax = dataPoints
-                            .SelectMany(line => line.Select(value => new { Value = value, LineIndex = Array.IndexOf(dataPoints, line)}))
-                            .Where(point => !hideLines.Contains(point.LineIndex))
-                            .Select(point => point.Value)
-                            .DefaultIfEmpty(0) // Provide a default value (e.g., 0) if the sequence is empty
-                            .Max();
+                        float currMaxVisibleVal = GetMaxVisibleValue();
+                        maxVisibleValueLineIndex = Array.IndexOf(maxLineValues, currMaxVisibleVal);
                     } else {
                         // line stopped beeing hidden
-                        float lineMaxVal = dataPoints[idx].Max();
-                        if (lineMaxVal > maxVisibleValue)
-                            maxVisibleValue = lineMaxVal;
+                        float lineMaxVal = GetMaxVisibleValue();
+                        if (lineMaxVal > maxLineValues[idx])
+                        {
+                            maxLineValues[idx] = lineMaxVal;
+                            if (idx != maxVisibleValueLineIndex && maxLineValues[maxVisibleValueLineIndex] < lineMaxVal)
+                            {
+                                maxVisibleValueLineIndex = idx;
+                            }
+                        }
                     }
                 }
             }
