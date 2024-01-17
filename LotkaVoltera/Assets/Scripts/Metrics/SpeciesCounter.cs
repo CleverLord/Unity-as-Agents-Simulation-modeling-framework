2f950cf795740090a4e6caf126ad4ceed645a609
Jan Karpiuk <karpiukjan@student.agh.edu.pl>
2024-01-17 17:56:49 +0100

    Add TODO for different aggregation methods

--- LotkaVoltera/Assets/Scripts/Metrics/SpeciesCounter.cs
+++ LotkaVoltera/Assets/Scripts/Metrics/SpeciesCounter.cs
@@ -78,8 +78,8 @@ public class SpeciesCounter : MonoBehaviour
         }
     }
 
-    // TODO: add different agregation methods: mean, median, min, max
-    public Dictionary<Species, int> getAgregatedData()
+    // TODO: implement other agregation methods: mean, median, min, max
+    public Dictionary<Species, int> getAggregatedData(AggregationMethod method = AggregationMethod.Mean)
     {
         // If any data in agregate buffor then agregate data
         if (speciesCountAgregator == null)
